b93cb4fd2d4d36997309f83dc6835d307ec22be9
MrPoxipol <michal.proszek@gmail.com>
2014-10-10 21:31:27 +0200

    Create class containing Color and its description

--- js/util.js
+++ js/util.js
@@ -7,3 +7,23 @@ function Color(r, g, b) {
 	this.g = g;
 	this.b = b;
 }
+
+/**
+	@brief Constructs a color object with name (description)
+	@param[i] color A Color object
+	@param[i] name 	String containing color description eg. purple
+*/
+function TileColor(color, name){
+	this.setColor = function(color) {
+		this.r = color.r;
+		this.g = color.g;
+		this.b = color.b;
+	};
+
+	this.setName = function(name) {
+		this.name = name;
+	};
+
+	this.setColor(color);
+	this.setName(name);
+}
